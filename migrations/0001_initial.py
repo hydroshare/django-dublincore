8397deb280d09cbb8e473409198755f9d73156eb
Michael J. Stealey <stealey@renci.org>
2014-11-20 11:57:14 -0500

    updated dublincore to django 1.7

--- migrations/0001_initial.py
+++ migrations/0001_initial.py
@@ -1,82 +1,51 @@
 # -*- coding: utf-8 -*-
-from south.utils import datetime_utils as datetime
-from south.db import db
-from south.v2 import SchemaMigration
-from django.db import models
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import django.db.models.deletion
 
 
-class Migration(SchemaMigration):
+class Migration(migrations.Migration):
 
-    def forwards(self, orm):
-        # Adding model 'QualifiedDublinCoreElement'
-        db.create_table(u'dublincore_qualifieddublincoreelement', (
-            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('object_id', self.gf('django.db.models.fields.CharField')(max_length=255)),
-            ('content_type', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['contenttypes.ContentType'])),
-            ('term', self.gf('django.db.models.fields.CharField')(max_length=4)),
-            ('qualifier', self.gf('django.db.models.fields.CharField')(max_length=40, null=True, blank=True)),
-            ('content', self.gf('django.db.models.fields.TextField')()),
-            ('updated_at', self.gf('django.db.models.fields.DateTimeField')(auto_now=True, blank=True)),
-            ('created_at', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
-        ))
-        db.send_create_signal(u'dublincore', ['QualifiedDublinCoreElement'])
+    dependencies = [
+        ('contenttypes', '0001_initial'),
+    ]
 
-        # Adding model 'QualifiedDublinCoreElementHistory'
-        db.create_table(u'dublincore_qualifieddublincoreelementhistory', (
-            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('object_id', self.gf('django.db.models.fields.CharField')(max_length=255)),
-            ('content_type', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['contenttypes.ContentType'])),
-            ('term', self.gf('django.db.models.fields.CharField')(max_length=4)),
-            ('qualifier', self.gf('django.db.models.fields.CharField')(max_length=40, null=True, blank=True)),
-            ('content', self.gf('django.db.models.fields.TextField')()),
-            ('updated_at', self.gf('django.db.models.fields.DateTimeField')(auto_now=True, blank=True)),
-            ('created_at', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
-            ('qdce', self.gf('django.db.models.fields.related.ForeignKey')(related_name='history', null=True, on_delete=models.SET_NULL, to=orm['dublincore.QualifiedDublinCoreElement'])),
-            ('qdce_id_stored', self.gf('django.db.models.fields.PositiveIntegerField')()),
-        ))
-        db.send_create_signal(u'dublincore', ['QualifiedDublinCoreElementHistory'])
-
-
-    def backwards(self, orm):
-        # Deleting model 'QualifiedDublinCoreElement'
-        db.delete_table(u'dublincore_qualifieddublincoreelement')
-
-        # Deleting model 'QualifiedDublinCoreElementHistory'
-        db.delete_table(u'dublincore_qualifieddublincoreelementhistory')
-
-
-    models = {
-        u'contenttypes.contenttype': {
-            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
-            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
-        },
-        u'dublincore.qualifieddublincoreelement': {
-            'Meta': {'ordering': "['term']", 'object_name': 'QualifiedDublinCoreElement'},
-            'content': ('django.db.models.fields.TextField', [], {}),
-            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenttypes.ContentType']"}),
-            'created_at': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'object_id': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
-            'qualifier': ('django.db.models.fields.CharField', [], {'max_length': '40', 'null': 'True', 'blank': 'True'}),
-            'term': ('django.db.models.fields.CharField', [], {'max_length': '4'}),
-            'updated_at': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'})
-        },
-        u'dublincore.qualifieddublincoreelementhistory': {
-            'Meta': {'ordering': "['term']", 'object_name': 'QualifiedDublinCoreElementHistory'},
-            'content': ('django.db.models.fields.TextField', [], {}),
-            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenttypes.ContentType']"}),
-            'created_at': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'object_id': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
-            'qdce': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'history'", 'null': 'True', 'on_delete': 'models.SET_NULL', 'to': u"orm['dublincore.QualifiedDublinCoreElement']"}),
-            'qdce_id_stored': ('django.db.models.fields.PositiveIntegerField', [], {}),
-            'qualifier': ('django.db.models.fields.CharField', [], {'max_length': '40', 'null': 'True', 'blank': 'True'}),
-            'term': ('django.db.models.fields.CharField', [], {'max_length': '4'}),
-            'updated_at': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'})
-        }
-    }
-
-    complete_apps = ['dublincore']
+    operations = [
+        migrations.CreateModel(
+            name='QualifiedDublinCoreElement',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('object_id', models.CharField(max_length=255)),
+                ('term', models.CharField(max_length=4, choices=[(b'AB', b'Abstract'), (b'BX', b'Box'), (b'BIB', b'BibliographicCitation'), (b'CN', b'Contributor'), (b'CVR', b'Coverage'), (b'CR', b'Creator'), (b'DT', b'Date'), (b'DTS', b'DateSubmitted'), (b'DC', b'DateCreated'), (b'DM', b'DateModified'), (b'DTS', b'DateSubmitted'), (b'DSC', b'Description'), (b'FMT', b'Format'), (b'ID', b'Identifier'), (b'LG', b'Language'), (b'PD', b'Period'), (b'PT', b'Point'), (b'PBL', b'Publisher'), (b'REF', b'Reference'), (b'REL', b'Relation'), (b'RT', b'Rights'), (b'SRC', b'Source'), (b'SUB', b'Subject'), (b'T', b'Title'), (b'TYP', b'Type')])),
+                ('content', models.TextField()),
+                ('updated_at', models.DateTimeField(auto_now=True)),
+                ('created_at', models.DateTimeField(auto_now_add=True)),
+                ('content_type', models.ForeignKey(to='contenttypes.ContentType')),
+            ],
+            options={
+                'ordering': ['term'],
+                'abstract': False,
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='QualifiedDublinCoreElementHistory',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('object_id', models.CharField(max_length=255)),
+                ('term', models.CharField(max_length=4, choices=[(b'AB', b'Abstract'), (b'BX', b'Box'), (b'BIB', b'BibliographicCitation'), (b'CN', b'Contributor'), (b'CVR', b'Coverage'), (b'CR', b'Creator'), (b'DT', b'Date'), (b'DTS', b'DateSubmitted'), (b'DC', b'DateCreated'), (b'DM', b'DateModified'), (b'DTS', b'DateSubmitted'), (b'DSC', b'Description'), (b'FMT', b'Format'), (b'ID', b'Identifier'), (b'LG', b'Language'), (b'PD', b'Period'), (b'PT', b'Point'), (b'PBL', b'Publisher'), (b'REF', b'Reference'), (b'REL', b'Relation'), (b'RT', b'Rights'), (b'SRC', b'Source'), (b'SUB', b'Subject'), (b'T', b'Title'), (b'TYP', b'Type')])),
+                ('content', models.TextField()),
+                ('updated_at', models.DateTimeField(auto_now=True)),
+                ('created_at', models.DateTimeField(auto_now_add=True)),
+                ('qdce_id_stored', models.PositiveIntegerField(help_text=b'Stores the id if the QulifiedDublinCoreElement the history points to is deleted.')),
+                ('content_type', models.ForeignKey(to='contenttypes.ContentType')),
+                ('qdce', models.ForeignKey(related_name='history', on_delete=django.db.models.deletion.SET_NULL, to='dublincore.QualifiedDublinCoreElement', null=True)),
+            ],
+            options={
+                'ordering': ['term'],
+                'abstract': False,
+            },
+            bases=(models.Model,),
+        ),
+    ]
